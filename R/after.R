345ff05c84618c0b8ba7babf7a1a03b6d434f89c
Anna Grytaas <grytaasanna@gmail.com>
2020-12-08 09:05:37 +0100

    first "after" function works on first tested dataset

--- R/after.R
+++ R/after.R
@@ -4,9 +4,9 @@
 #'
 #' @return description The function creates a dataframe that is a wider version of the one created with convertxrf().
 #'
-#' @param projectpath The file created by convertxrf().
+#' @param projectpath The CSV file created with convertxrf().
 #'
-#' @importFrom dplyr select
+#' @importFrom dplyr select filter
 #' @importFrom tidyr pivot_wider
 #' @importFrom magrittr %>%
 #' @importFrom readr read_csv
@@ -20,12 +20,38 @@ widen <- function(projectpath) {
   project.df <- readr::read_csv(file = projectpath)
 
   projectwide.df <- project.df %>%
+    dplyr::filter(!Filter_blank %in% "blank") %>%
     dplyr::select(-c(1, Detection_limit)) %>%
     tidyr::pivot_wider(names_from = Element, values_from = Concentration)
-
-  projectwide.df <- as.data.frame(projectwide.df)
 
   return(projectwide.df)
 
 }
 
+
+#' Widening the dataframe and removing concentrations below the detection limits
+#'
+#' @description This function will both widen the dataframe so each element has its own column, and remove concentrations that are below the detection limits.
+#'
+#' @return description The function creates a dataframe that is a wider version of the one created with convertxrf(), and where values below the detection limits are removed.
+#'
+#' @param projectpath The CSV file created with convertxrf().
+#'
+#' @importFrom dplyr filter select
+#' @importFrom tidyr pivot_wider
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#'
+#' @export
+
+widen_above <- function(projectpath) {
+
+  project.df <- readr::read_csv(file = projectpath)
+
+  projectabove.df <- project.df %>%
+    dplyr::filter(Concentration > Detection_limit) %>%
+    dplyr::select(-Detection_limit) %>%
+    tidyr::pivot_wider(names_from = Element, values_from = Concentration)
+
+}
--- firsttest.R
+++ firsttest.R
@@ -3,7 +3,9 @@ projectfile.df <- importxrf(datapath = "xrf_rawdata.txt", infopath = "xrf_projec
 
 project.df <- convertxrf(setuppath = "xrf_setup.xlsx", year = "2019", first_element = "C", last_element = "As")
 
-widen(projectpath = "project_long.csv")
+wide.df <- widen(projectpath = "project_long.csv")
+
+wideabove.df <- widen_above(projectpath = "project_long.csv")
 
 
 
@@ -17,12 +19,13 @@ project1.df <- convertxrf(setuppath = "xrf_setup.xlsx", year = "2019", first_ele
 
 
 
-
-projectwide.df <- project2.df %>%
-  dplyr::select(-Detection_limit, -1) %>%
-  tidyr::pivot_wider(names_from = Element, values_from = Concentration)
-
 projecmeans.df <- projectwide.df %>%
   dplyr::group_by(Treatment, Day) %>%
   dplyr::summarise_if(is.numeric, mean) %>%
   dplyr::select(Day, Treatment, C:As)
+
+
+averagesaug2.df <- project.aug.df %>%
+  filter(Value > Detection_lim) %>%
+  select(-Detection_lim) %>%
+  pivot_wider(names_from = Element, values_from = Value)
